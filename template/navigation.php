962bab1ad1828cb420feaea224e5d8ed0e7cc828
Lukas <lukas.scharmitzer@gmail.com>
2018-06-21 12:58:02 +0200

    added x2 icon in menue

--- template/navigation.php
+++ template/navigation.php
@@ -6,7 +6,7 @@
 <div class="top-bar" id="nav">
   <div class="top-bar-left">
     <ul class="dropdown menu" data-dropdown-menu>
-      <li class="menu-text"><a href="<?php echo $pathAPP; ?>index.php"><i class="fas fa-home  " style="color: #2a6182;"></i></a></li>
+      <li class="menu-text"><a href="<?php echo $pathAPP; ?>index.php"><i class="fas fa-home fa-2x " style="color: #2a6182;"></i></a></li>
       <li><a href="<?php echo $pathAPP; ?>private/crudBoard.php">Dashboard</a></li>
       <li><a href="<?php echo $pathAPP; ?>aboutUs.php">About us</a></li>
       <li><a href="<?php echo $pathAPP; ?>contact.php">Kontakt</a></li>
