66a4a8dd60c701f6a8539746264c958a0d41982d
Lukas <lukas.scharmitzer@gmail.com>
2018-07-13 13:01:02 +0200

    created database connection object

--- config.php
+++ config.php
@@ -6,4 +6,7 @@ $pathAPP = "/broomCall/";
 $appID = 25082017;
 $titleAPP = "broomCall V1"; 
 
+
+$conn = new PDO("mysql:host=localhost;dbname=1broomCall", "lukas", "123456789"); 
+$conn->exec("set names utf8");
 ?>
