afddc7eb45f066308fd1b142e2cb63111405aafc
Lukas <lukas.scharmitzer@gmail.com>
2018-07-13 13:02:12 +0200

    created static files for authorized users

---
+++ private/cleanLevel/index.php
@@ -0,0 +1,19 @@
+<?php include_once "../../config.php"; ?>
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+  <?php include_once "../../template/head.php"; ?>
+  </head>
+  <body>
+
+  <?php include_once "../../template/navigation.php"; ?>  
+  <br> <hr>
+  <?php print_r($_SESSION);  ?>
+  <br>
+    CLEAN LEVELS
+  <?php include_once "../../template/scripts.php"; ?>
+
+  <?php include_once "../../template/footer.php"; ?>
+  
+  </body>
+</html>
