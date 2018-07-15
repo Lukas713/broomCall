e92a829804588f16a916057d1278d03fc662366d
Lukas <lukas.scharmitzer@gmail.com>
2018-07-15 13:23:06 +0200

    Changed table header

--- private/cleanLevel/index.php
+++ private/cleanLevel/index.php
@@ -24,37 +24,37 @@ if(!isset($_SESSION[$appID."operater"])){
   ?>
 
   <div class="grid-container"> 
-  <h3>Clean levels</h3><hr>
-  <a href="new.php" class="button">Create new</a> 
-  <div class="grid-x grid-margin-x">
-      <table>
-        <thead>
-          <tr style="color:#1779ba;">
-            <th>Level name</th>
-            <th>Price coefficient</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php foreach($result as $row): ?>
-            <tr>
-              <td><?php echo $row->levelName; ?></td>
-              <td><?php echo $row->priceCoeficient; ?></td>
-              <td>
-              <a onclick="return confirm('Delete -><?php echo $row->levelName; ?>?')" href="delete.php?id=<?php echo $row->id; ?>">
-               <i class="fas fa-2x fa-trash-alt"></i>
-              </a>  
-                <a href="rewrite.php?id=<?php echo $row->id; ?>"><i class="fas fa-2x fa-edit"></i></a>
-              </td>
+    <h3>Clean levels</h3><hr>
+    <a href="new.php" class="button">Create new</a> 
+    <div class="grid-x grid-margin-x">
+        <table>
+          <thead>
+            <tr style="color:#1779ba;">
+              <th>Level name</th>
+              <th>Price coefficient</th>
+              <th>Action</th>
             </tr>
-        <?php endforeach; ?>
-        </tbody>
-    </table>
+          </thead>
+          <tbody>
+            <?php foreach($result as $row): ?>
+              <tr>
+                <td><?php echo $row->levelName; ?></td>
+                <td><?php echo $row->priceCoeficient; ?></td>
+                <td>
+                <a onclick="return confirm('Delete -><?php echo $row->levelName; ?>?')" href="delete.php?id=<?php echo $row->id; ?>">
+                <i class="fas fa-2x fa-trash-alt"></i>
+                </a>  
+                  <a href="rewrite.php?id=<?php echo $row->id; ?>"><i class="fas fa-2x fa-edit"></i></a>
+                </td>
+              </tr>
+          <?php endforeach; ?>
+          </tbody>
+      </table>
+    </div>
   </div>
-</div>
-  <?php include_once "../../template/scripts.php"; ?>
+    <?php include_once "../../template/scripts.php"; ?>
 
-  <?php include_once "../../template/footer.php"; ?>
+    <?php include_once "../../template/footer.php"; ?>
   
   </body>
 </html>
