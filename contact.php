a43a4618230cf059725733d229a47486ac08e01b
Lukas <lukas.scharmitzer@gmail.com>
2018-07-15 16:51:19 +0200

    added contact informations

--- contact.php
+++ contact.php
@@ -4,8 +4,8 @@
   <head>
     <?php include_once "template/head.php"; ?>
     <style>
-      #map{
-        background-color: grey; 
+      .contact {
+        margin-left: 1rem; 
       }
     </style>
   </head>
@@ -14,23 +14,59 @@
   <?php include_once "template/navigation.php"; ?>  
 
   <div class="grid-container">
-  <h3>Map</h3><hr>
-    <div class="grid-x grid-margin-x">
-    <div id="googleMap" style="width:100%;height:400px;"></div>
-        <script>
-            function myMap() {
-              var myCenter = new google.maps.LatLng(45.560455,18.680983);
-              var mapCanvas = document.getElementById("googleMap");
-              var mapOptions = {center: myCenter, zoom: 15};
-              var map = new google.maps.Map(mapCanvas, mapOptions);
-              var marker = new google.maps.Marker({position:myCenter});
-              marker.setMap(map);
-            }   
-
-
-
-        </script>
+    <h3>Map</h3>
+      <div class="grid-x">
+        <div id="googleMap" style="width:100%;height:400px;"></div>
+          <script>
+              //funcion for google maps
+              function myMap() {
+                var myCenter = new google.maps.LatLng(45.560455,18.680983);
+                var mapCanvas = document.getElementById("googleMap");
+                var mapOptions = {center: myCenter, zoom: 15};
+                var map = new google.maps.Map(mapCanvas, mapOptions);
+                var marker = new google.maps.Marker({position:myCenter});
+                marker.setMap(map);
+              }  
+          </script>
+      </div><hr><br>
+      <h4>Contact informations</h4>
+    <div class="grid-x">
+          <div class="cell" style="width:2rem">
+            <i class="fas fa-map-marker-alt" style="color:green"></i>
+          </div>
+          <div class="cell medium-4 small-4">
+            Ulica Lorenza Jegera 5<br>
+            31000, Osijek<br>
+          </div>
     </div> 
+    <br>
+    <div class="grid-x">
+          <div class="cell" style="width:2rem">
+          <i class="fas fa-phone" style="color:green"></i>
+          </div>
+          <div class="cell medium-4 small-4">
+            +385 91 551 7903<br>
+            +385 31 551 932<br>
+          </div>
+    </div>
+    <br>
+    <div class="grid-x">
+          <div class="cell" style="width:2rem">
+          <i class="fas fa-envelope" style="color:green"></i>
+          </div>
+          <div class="cell medium-4 small-4">
+            lukas.scharmitzer@gmail.com<br>
+          </div> 
+    </div>
+    <br>
+    <div class="grid-x">
+          <div class="cell" style="width:2rem">
+          <i class="fas fa-globe" style="color:green"></i>
+          </div>
+          <div class="cell medium-4 small-4">
+            www.broomCall.com<br>
+          </div>
+    </div>
   </div>
 
   <?php include_once "template/scripts.php"; ?>
