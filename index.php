9be704a3f4071d51502b42f48696261a1eae5c09
Lukas <lukas.scharmitzer@gmail.com>
2018-07-18 11:37:29 +0200

    added Open Graph Protocol

--- index.php
+++ index.php
@@ -1,21 +1,19 @@
 <?php include_once "config.php"; ?>
 <!doctype html>
-<html class="no-js" lang="en" dir="ltr">
+<html prefix="og: http://ogp.me/ns#" class="no-js" lang="en" dir="ltr">
   <head>
-  <?php include_once "template/head.php"; ?>
+    <?php include_once "template/head.php"; ?>
   </head>
   <body>
 
-  <?php include_once "template/navigation.php"; ?>  
-  <br> <hr>
-  <?php print_r($_SESSION);  ?>
-  <br>
-  <pre>
-    <?php print_r($_SERVER);  ?>
-  </pre>
-  <?php include_once "template/scripts.php"; ?>
+    <?php include_once "template/navigation.php"; ?>  
+    <br> <hr>
+    <div class="grid-container">
+      Indeks
+    </div>
+    <?php include_once "template/scripts.php"; ?>
 
-  <?php include_once "template/footer.php"; ?>
+    <?php include_once "template/footer.php"; ?>
   
   </body>
 </html>
--- template/head.php
+++ template/head.php
@@ -2,6 +2,10 @@
 <meta http-equiv="x-ua-compatible" content="ie=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title><?php echo $titleAPP;?></title>
+<meta property="og:title" content="BroomCall" />
+<meta property="og:type" content="cleaning.service" />
+<meta property="og:url" content="http://lukas713.byethost17.com/broomCall/" />
+<meta property="og:image" content="http://www.titanmachinery.com/images/layout/equipment/default.jpg" />
 <link rel="stylesheet" href="<?php echo $pathAPP;?>css/foundation.css">
 <link rel="stylesheet" href="<?php echo $pathAPP;?>css/app.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
