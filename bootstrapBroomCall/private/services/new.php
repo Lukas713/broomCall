e8773fb6fa2bab0c16a173633e9b0b604e019ddd
Lukas <lukas.scharmitzer@gmail.com>
2018-08-24 17:11:20 +0200

    generic tables

--- bootstrapBroomCall/private/services/new.php
+++ bootstrapBroomCall/private/services/new.php
@@ -3,13 +3,42 @@ include_once "../../config.php";
 if(!isset($_SESSION[$appID."operater"])){
   header('location:'.$pathAPP.'logout.php');
 } 
-if(isset($_POST["add"])){
-    $query = $conn->prepare("insert into services(id, serviceName, price) values
-                            (null, :serviceName, :price)");
-    unset($_POST["add"]);
-    $query->execute($_POST); 
-    header("location: index.php"); 
+
+$query = $conn->prepare("describe services");
+$query->execute(); 
+$result = $query->fetchAll(PDO::FETCH_OBJ);
+$array = array(); 
+
+foreach($result as $row){
+    if($row->Field == "id"){
+        continue;
+    }
+    $array[] = $row->Field; 
 }
+
+
+if(isset($_POST["add"])){
+
+    $firstListBracket="";
+    $secondListBracket="";
+
+    foreach($array as $row){
+        $firstListBracket.= $row . ",";
+        $secondListBracket.=":" . $row . ",";
+    }
+
+    $firstListBracket=substr($firstListBracket,0,strlen($firstListBracket)-1);
+    $secondListBracket=substr($secondListBracket,0,strlen($secondListBracket)-1);
+
+    $query = $conn->prepare("insert into services (".$firstListBracket.") values
+                            (".$secondListBracket.")");
+
+    
+    unset($_POST["add"]);                       
+    $query->execute($_POST);
+    header("location: index.php");   
+}
+
 ?>
 
 <!doctype html>
@@ -25,14 +54,14 @@ if(isset($_POST["add"])){
   <h3>New service</h3><hr>
       <div class="row justify-content-md-center"> 
       <form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
+      <?php
+      foreach($array as $row):
+      ?>
         <div class="form-group">
-            <label for="serviceName">Service name</label>
-            <input type="text" class="form-control" id="serviceName" name="serviceName">
+            <label for="<?php echo $row;?>">Service name</label>
+            <input type="text" class="form-control" id="<?php echo $row;?>" name="<?php echo $row;?>">
         </div>
-        <div class="form-group">
-            <label for="price">Price</label>
-            <input type="number" step="0.01" class="form-control" id="price" name="price">
-        </div>
+      <?php endforeach;?>
         <input type="submit" class="btn btn-primary" value="Submit" name="add">
         <a href="index.php" class="btn btn-danger">Cancel</a>
     </form>
