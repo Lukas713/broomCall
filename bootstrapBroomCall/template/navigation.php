eb6404475f771522cb354c42f639e4892286b8d7
Lukas <lukas.scharmitzer@gmail.com>
2018-08-25 18:47:40 +0200

    created SVG logo

--- bootstrapBroomCall/template/navigation.php
+++ bootstrapBroomCall/template/navigation.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-info text-white">
-  <a class="navbar-brand" href="index.php">Logo</a>
+<a class="navbar-brand" href="index.php"><img src="<?php echo $pathAPP;?>img/logo.svg" alt="logoBroomCall" style="height:3rem; width:5rem;"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
