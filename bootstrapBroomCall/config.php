c0dbb6b82f3dfae96086d793d7ba2d2e3f61fb80
Lukas <lukas.scharmitzer@gmail.com>
2018-08-30 11:51:00 +0200

    switch case for byethost HTTPhost and localhost

--- bootstrapBroomCall/config.php
+++ bootstrapBroomCall/config.php
@@ -3,10 +3,27 @@ session_start();
 
 include_once "functions.php";
 
-$pathAPP = "/bootstrapBroomCall/" ;
 $titleAPP = "BroomCall V 1.0 - b" ;
 $appID = 20082018;
 
-$conn = new PDO("mysql:host=localhost;dbname=1broomCall", "lukas", "123456789");
+switch($_SERVER["HTTP_HOST"]){
+    case "lukas713.byethost17.com":
+    $pathAPP = "/bootstrapBroomCall/" ;
+    $server = "sql210.byethost.com";
+    $dbName = "b17_22272873_broomCall";
+    $userName = "b17_22272873";
+    $password = "broomcall12345";
+    break;
+    case "localhost":
+    $pathAPP = "/bootstrapBroomCall/" ;
+    $server = "localhost";
+    $dbName = "1broomCall";
+    $userName = "lukas";
+    $password = "123456789";
+    break;
+}
+
+$conn = new PDO("mysql:host=$server;dbname=$dbName", "$userName", "$password");
+
 
 $conn->exec("set names utf8");
