563c53064b4205f2227f22bc1e1329c041f33d45
Lukas <lukas.scharmitzer@gmail.com>
2018-09-02 10:25:47 +0200

    delete agreement option

---
+++ bootstrapBroomCall/private/agreements/delete.php
@@ -0,0 +1,20 @@
+<?php
+include_once "../../config.php";
+if(!isset($_SESSION[$appID."operater"])){
+  header('location:'.$pathAPP.'logout.php');
+} 
+
+try{
+    $conn->beginTransaction();
+
+    $query=$conn->prepare("DELETE from agreement where id=:id");
+    $query->execute($_GET);
+
+    $conn->commit(); 
+    header("location: index.php"); 
+}catch(PDOexeption $e){
+    $conn->rollBck(); 
+}
+
+
+?>
