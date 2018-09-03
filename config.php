16abdb345b4bb491ba9114fb86aab67710244b1d
Lukas <lukas.scharmitzer@gmail.com>
2018-09-03 15:12:49 +0200

    if statement for switching between localhost and byethost

--- config.php
+++ config.php
@@ -6,7 +6,24 @@ $pathAPP = "/broomCall/";
 $appID = 25082017;
 $titleAPP = "broomCall V1";  
 
-$conn = new PDO("mysql:host=localhost;dbname=1broomCall", "lukas", "123456789");
+switch($_SERVER["HTTP_HOST"]){
+    case "lukas713.byethost17.com":
+    $pathAPP = "/broomCall/" ;
+    $server = "sql210.byethost.com";
+    $dbName = "b17_22272873_broomCall";
+    $userName = "b17_22272873";
+    $password = "broomcall12345";
+    break;
+    case "localhost":
+    $pathAPP = "/BroomCall/" ;
+    $server = "localhost";
+    $dbName = "1broomCall";
+    $userName = "lukas";
+    $password = "123456789";
+    break;
+}
+
+$conn = new PDO("mysql:host=$server;dbname=$dbName", "$userName", "$password");
 
 $conn->exec("set names utf8");
 ?>
