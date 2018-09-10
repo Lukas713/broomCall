b95fed202739b0f23afa7d1d6838754e7ae83485
Lukas <lukas.scharmitzer@gmail.com>
2018-09-10 14:24:27 +0200

    approving agreements and choosing squads

--- private/controlBoard.php
+++ private/controlBoard.php
@@ -1,59 +1,79 @@
 <?php include_once "../config.php"; ?>
-<!doctype html>
-<html class="no-js" lang="en" dir="ltr">
-  <head>
-    <?php include_once "../template/head.php"; ?>
-  </head>
-  <body>
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include_once "../template/head.php"; ?>
+    </head>
 
-    <?php include_once "../template/navigation.php"; ?>  
-    <br> <hr>
-          <div class="grid-container">
-            <!-- prepare sql query, execute, fetch as object and display the result  -->
-            <?php
-            $query =  $conn->prepare("select * from users where ;");
-            $query->execute(); 
-            $result = $query->fetchAll(PDO::FETCH_OBJ); 
+    <body>
+        <?php include_once "../template/navigation.php"; ?>
+        <br>
+        <div class="container">
+            <h3>Pending agreements</h3><hr>
+                <?php
+                $query = $conn->prepare("SELECT a.id, concat(c.firstName, ' ', c.lastName) as person, a.serviceDate, a.city, a.adress, b.phoneNumber, d.levelName, d.priceCoeficient, e.serviceName, e.price, (d.priceCoeficient * e.price) as total
+                                            from agreement a
+                                            inner join users b on a.users = b.id
+                                            inner join person c on b.person = c.id
+                                            inner join cleanlevel d on a.cleanlevel = d.id
+                                            inner join services e on a.services = e.id
+                                            where approved = false;");
+                $query->execute();
+                $result=$query->fetchAll(PDO::FETCH_OBJ);   
+                ?>
+                <div class="row justify-content-center">
+                    <?php foreach($result as $row): ?>
+                    <div class="card-deck mb-4 text-center">
+                            <div class="card md-4 shadow-sm">
+                                <div class="card-header">
+                                    <h4 class="my-0 font-weight-normal"><?php echo $row->person;?></h4>
+                                </div>
+                                <div class="card-body">
+                                    <h1 class="card-title pricing-card-title"><?php echo $row->total;?> €</h1>
+                                    <ul class="list-unstyled mt-3 mb-4">
+                                    <li><b>Date and time: </b><?php echo $row->serviceDate; ?></li>
+                                    <li><b>Adress: </b><?php echo $row->city.', '.$row->adress; ?></li>
+                                    <li><b>Phone: </b> <?php echo $row->phoneNumber; ?></li>
+                                    <li><b>Clean level: </b><?php echo $row->levelName.': '.$row->priceCoeficient; ?></li>
+                                    <li><b>Service: </b><?php echo $row->serviceName.': '.$row->price; ?> €</li>
+                                    <li><b>Chose a squad:</b></li>
+                                    <li>
+                                    <div class="input-group mb-3 justify-content-center" id="approveItem">
+                                        <?php
+                                            $query = $conn->prepare("select * from squad");
+                                            $query->execute();
+                                            $result = $query->fetchAll(PDO::FETCH_OBJ);
+                                            foreach($result as $row){   
+                                                echo '<input type="button" class="btn btn-secondary" style=background-color:'.$row->squadColor.' value='.$row->squadNumber.' id="approveButton">';
+                                            } 
+                                        ?>
+                                    </div>
+                                    </li>
+                                    </ul><hr>
+                                    <button type="button" class="btn btn-success">Approve</button>
+                                </div>
+                            </div>
+                    </div>
+                    <?php endforeach;?> 
+                    </div> 
+                </div>
+        </div>           
+        <script>
+         $('#document').ready(function(){
+             $('#approveButton').click(function(){
+                 $.ajax({
+                    url: 'approve.php',
+                    data:{$('#approveButton').val()};
+                    success: function(result){
+                        alert(result)
+                    }
+                 })
+             });
+         });
+        </script>
 
-            
-            ?>
-          </div>
+        <?php include_once "../template/scripts.php"; ?>
 
-          <div class="grid-container"> 
-  <h3>Users</h3><hr> 
-  <div class="grid-x grid-margin-x">
-      <table>
-        <thead>
-          <tr style="color:#1779ba;">
-            <th>First name</th>
-            <th>Last name</th>
-            <th>Email</th>
-            <th>Phone Number</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php foreach($result as $row): ?>
-            <tr>
-              <td><?php echo $row->firstName; ?></td>
-              <td><?php echo $row->lastName; ?></td>
-              <td><?php echo $row->email; ?></td>
-              <td><?php echo $row->phoneNumber; ?></td>
-              <td>
-              <a onclick="return confirm('Delete -><?php echo $row->firstName; ?>?')" href="delete.php?id=<?php echo $row->id; ?>">
-               <i class="fas fa-2x fa-trash-alt"></i>
-              </a>  
-                <a href="rewrite.php?id=<?php echo $row->id; ?>"><i class="fas fa-2x fa-edit"></i></a>
-              </td>
-            </tr>
-        <?php endforeach; ?>
-        </tbody>
-    </table>
-  </div>
-</div>
-    <?php include_once "../template/scripts.php"; ?>
-
-    <?php include_once "../template/footer.php"; ?>
-  
-  </body>
+        <?php include_once "../template/footer.php"; ?>
+    </body>
 </html>
