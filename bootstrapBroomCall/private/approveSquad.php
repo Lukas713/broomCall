a38be76e6380b5e49ee49260e1697bde048ebde4
Lukas <lukas.scharmitzer@gmail.com>
2018-09-19 10:45:42 +0200

    setting squad within agreement

--- bootstrapBroomCall/private/approveSquad.php
+++ bootstrapBroomCall/private/approveSquad.php
@@ -1,7 +1,14 @@
 <?php 
 include_once "../config.php";
 
-if(!isset($_SESSION[$appID."operater"])){
+if(!isset($_SESSION[$appID."admin"])){
     return;
- 
 }
+
+
+$query = $conn->prepare("UPDATE agreement
+                        set approved = true,
+                        squad = :squadNumber
+                        where id=:agreementID");
+$query->execute($_POST);
+echo "good job"; 
--- bootstrapBroomCall/private/controlBoard.php
+++ bootstrapBroomCall/private/controlBoard.php
@@ -38,25 +38,24 @@
                                     <li><b>Service: </b><?php echo $row->serviceName.': '.$row->price; ?> €</li>
                                     <li><b>Chose a squad:</b></li>
                                     <li>
-                                    <div class="input-group mb-3 justify-content-center" id="approveItem">
-                                        <?php
-                                        
-                                            $query = $conn->prepare("select * from squad");
-                                            $query->execute();
-                                            $result = $query->fetchAll(PDO::FETCH_OBJ); 
-                                            foreach($result as $li){  
-                                                
-                                                if($li->squadNumber == 4){
-                                                    echo "";
-                                                }else {
-                                                    echo '<input type="button" class="btn btn-secondary squadColor" style=background-color:'.$li->squadColor.' value='.$li->squadNumber.'>';
-                                                }
-                                            } 
-                                        ?>
-                                    </div>
+                                        <div class="input-group mb-3 justify-content-center" id="approveItem">
+                                            <?php
+                                            
+                                                $query = $conn->prepare("select * from squad");
+                                                $query->execute();
+                                                $result = $query->fetchAll(PDO::FETCH_OBJ); 
+                                                foreach($result as $li){  
+                                                    
+                                                    if($li->squadNumber == 4){
+                                                        echo "";
+                                                    }else {
+                                                        echo '<input type="button" class="btn btn-secondary squadColor" style=background-color:'.$li->squadColor.' value='.$li->squadNumber.' id='.$li->squadNumber.'-'.$row->id.'>';
+                                                    }
+                                                } 
+                                            ?>
+                                        </div>
                                     </li>
-                                    </ul><hr>
-                                        <input type="button" class="btn btn-success agreementID" value="<?php echo $row->id;?>">
+                                    </ul>   
                                 </div>
                             </div>
                     </div>
@@ -68,8 +67,27 @@
         <?php include_once "../template/footer.php"; ?>
 
         <script>
-    
 
+            $(".squadColor").click(function(){
+                var o = $(this); 
+                var x = $(this).attr('id');
+                x = x.split('-'); 
+
+                $.ajax({
+                    type: "POST",
+                    url: "approveSquad.php",
+                    data: "squadNumber=" + x[0] + "& agreementID=" + x[1], 
+                    success: function(serverReturn){
+                        if(serverReturn === "good job"){
+                        
+                            o.parent().parent().parent().parent().parent().remove(); 
+                        }
+                    }
+                });
+
+            });
+
+            
 
         </script>
     </body>
