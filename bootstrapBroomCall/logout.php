bfb3cad3095459c21f4dc31b8c96577b52fdfe2b
Lukas <lukas.scharmitzer@gmail.com>
2018-09-20 20:20:31 +0200

    changed logout

--- bootstrapBroomCall/logout.php
+++ bootstrapBroomCall/logout.php
@@ -1,18 +1,5 @@
 <?php
 include_once "config.php";
-if(isset($_SESSION[$appID."operater"])){
-
-    unset($_SESSION[$appID."operater"]);
-
-}else if(isset($_SESSION[$appID."admin"])){
-
-    unset($_SESSION[$appID."admin"]);
-
-}else if(isset($_SESSION[$appID."user"])){
-
-    unset($_SESSION[$appID."user"]);
-
-}
-
+$_SESSION=[]; 
 header("location: login.php"); 
 ?>
