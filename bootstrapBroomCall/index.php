3967f0798dfdced92c0f49b675ffcc775ea5016c
Lukas <lukas.scharmitzer@gmail.com>
2018-09-26 14:13:40 +0200

    modal window for ordering

--- bootstrapBroomCall/index.php
+++ bootstrapBroomCall/index.php
@@ -3,16 +3,51 @@
 <html>
     <head>
         <?php include_once "template/head.php"; ?>
+        <style>
+
+        </style>
     </head>
 
     <body>
         <?php include_once "template/navigation.php"; ?>
         <br> <hr>
         <div class="grid-container">
-        <?php print_r($_SESSION);   ?>
-        </div>
-        <?php include_once "template/scripts.php"; ?>
+            <div class="row justify-content-center">
+                <div class="col col-md-3">
+                    <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                        Order cleaning service
+                        </button>
 
+                            <!-- Modal -->
+                            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="orderAgreement">Modal title</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                <?php
+                                            print_r($_SESSION); 
+                                    ?>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary">Create order</button>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
         <?php include_once "template/footer.php"; ?>
+        <?php include_once "template/scripts.php"; ?>
+        <script>
+           
+        </script>
     </body>
 </html>
