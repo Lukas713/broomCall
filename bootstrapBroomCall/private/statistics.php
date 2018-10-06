1c39971b6efe3adfbfcba9b9a3e89351441e10f1
Lukas <lukas.scharmitzer@gmail.com>
2018-10-06 12:12:41 +0200

    first pie chart completed

--- bootstrapBroomCall/private/statistics.php
+++ bootstrapBroomCall/private/statistics.php
@@ -15,7 +15,9 @@ if(!isset($_SESSION[$appID."admin"])){
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col col-md-6">
-                    <h3>Pie chart</h3>
+
+                    <div id="container" style="min-width: 310px; height: 400px; max-width: 600px; margin: auto"></div>
+
                 </div>
                 <div class="col col-md-6">
                     One of three columns
@@ -38,7 +40,7 @@ if(!isset($_SESSION[$appID."admin"])){
                     type: 'pie'
                 },
                 title: {
-                    text: 'Clean level agreements within 2017-2018 years'
+                    text: 'Usage of clean levels, pie chart'
                 },
                 tooltip: {
                     pointFormat: '{series.name}: <b>{point.y}</b>'
@@ -48,19 +50,28 @@ if(!isset($_SESSION[$appID."admin"])){
                         allowPointSelect: true,
                         cursor: 'pointer',
                         dataLabels: {
-                            enabled: false,
-                            format: '<b>{point.name}</b>: {point.y}',
+                            enabled: true,
+                            format: '<b>{point.name}</b>: {point.x} %',
                             style: {
                                 color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
                             }
-                        },
-                        showInLegend: true
+                        }
                     }
                 },
                 series: [{
                     name: 'Clean levels',
                     colorByPoint: true,
-                    data: 
+                    data: <?php
+                        $query = $conn->prepare("select b.levelName as name, count(a.cleanLevel) as y, sum(a.cleanLevel),
+                                                (sum(a.cleanLevel) / (select sum(a.cleanLevel) from agreement a))*100 as x 
+                                                from cleanlevel b 
+                                                inner join agreement a on a.cleanLevel = b.id
+                                                group by name
+                                                ");
+                        $query->execute();
+                        $result = $query->fetchAll(PDO::FETCH_OBJ);
+                        echo json_encode($result, JSON_NUMERIC_CHECK);
+                    ?> 
                 }]
             });
         </script>
